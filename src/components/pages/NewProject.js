cb3fe11d7d527993122ec2a2ea7bd4e69e6f4112
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-03 20:37:19 -0300

    criando pagina de forms

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,12 @@
+import ProjectForm from '../project/ProjectForm'
+import style from './NewProject.module.css'
+
 function NewProject(){
     return (
-        <div>
-            <h1>NewProject</h1>
+        <div className={style.newProject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
         </div>
     )
 }

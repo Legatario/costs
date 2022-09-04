ac6292f9fca714aac87e0abda99f12554f873469
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-03 21:57:36 -0300

    Conectando com o Api pelo react

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,20 +1,45 @@
-function ProjectForm(){
+import {useEffect, useState} from 'react'
+
+import Input from '../form/Input'
+import Select from '../form/Select'
+import Submit from '../form/Submit'
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+   useEffect(()=>{
+        fetch("http://localhost:5000/categories",{
+            method:"GET",
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data)=>{
+            setCategories(data)
+        })
+        .catch((err)=> console.log(err))
+   },[])
+
+
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto"/>
-            </div>
-            <div>
-                <input type="numer" placeholder="Insira o orçamento total"/>
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disable selected> Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
+        <form className={styles.form}>
+            <Input 
+            type="text" 
+            name="name" 
+            placeholder="Insira o nome do projeto" 
+            text="Nome do projeto" 
+            />
+             <Input 
+            type="Number" 
+            name="budget" 
+            placeholder="Insira o orçamento total" 
+            text="Orçamento do projeto" 
+            />
+            <Select name="category_id" text="Selecione a categoria"  options={categories} />
+            <Submit text={btnText}/>
         </form>
     )
 }

d2f503b91ee931d256a5d5d108c43a866b0535e7
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-04 18:12:34 -0300

    criando pagina para projeto:id

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/company" element={<Company />} />
           <Route exact path="/newproject" element={<NewProject />} />
           <Route exact path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<Project />} />
       </Routes>
         </Container>
       <Footer />
